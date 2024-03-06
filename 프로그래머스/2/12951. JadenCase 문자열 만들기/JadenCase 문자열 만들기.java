11d37a8f18472d1bd2a1b7d154b699b27ce4e4ba
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-06 18:16:47 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.48 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+		String[] words = s.toLowerCase().split("");
+		
+		boolean flag = true;
+		
+		for (String word : words) {
+			answer += flag ? word.toUpperCase() : word;
+			flag = word.equals(" ") ? true : false;
+		}
+		
+		return answer;
+    }
+}
+
