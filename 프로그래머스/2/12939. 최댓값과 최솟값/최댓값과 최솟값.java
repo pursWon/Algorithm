0cf1366fac2d71e0c67301e5214fab39d0b2814d
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-11 23:50:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.90 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String s) {
-        String[] tmp = s.split(" ");
-        int min, max, n;
-        min = max = Integer.parseInt(tmp[0]);
+        String[] arr = s.split(" ");
+        int[] numbers = new int[arr.length];
         
-        for (int i = 1; i < tmp.length; i++) {
-                n = Integer.parseInt(tmp[i]);
-            if(min > n) min = n;
-            if(max < n) max = n;
+        for (int i = 0; i < arr.length; i++) {
+            numbers[i] = Integer.parseInt(arr[i]);
         }
- 
+
+        Arrays.sort(numbers);
+        
+        int min = numbers[0];
+        int max = numbers[numbers.length - 1];
+        
         return min + " " + max;
     }
 }
