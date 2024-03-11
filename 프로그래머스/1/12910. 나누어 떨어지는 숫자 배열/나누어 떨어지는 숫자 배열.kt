3aea854190ca96a33f57738eadbd67bf1ef28a7e
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-11 23:47:18 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 6.03 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.kt
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.kt
@@ -1,21 +1,21 @@
 class Solution {
-    fun solution(arr: IntArray, divisor: Int): IntArray {
+    fun solution(arr: IntArray, div: Int): IntArray {
     var answer = intArrayOf()
-    var bbc = mutableListOf<Int>()
+    var myList = mutableListOf<Int>()
     
     for(i in 0 ..arr.size-1) {
-        if(arr[i] % divisor == 0) {
-            bbc.add(arr[i]) // 나누어 떨어지는 값은 List에 저장
+        if(arr[i] % div == 0) {
+            myList.add(arr[i]) // 나누어 떨어지는 값은 List에 저장
         }
+        
         else continue
     }
     
-    if(bbc.size == 0 ) {
-    bbc.add(-1) // 나누어 떨어지는 값이 없으면 -1로 반환
-    }
-    bbc.sort() //List내 데이터를 오름차순으로 정렬 
+    if(myList.size == 0 ) { myList.add(-1) }
+    // 나누어 떨어지는 값이 없으면 -1로 반환
     
-    answer = bbc.toIntArray()
+    myList.sort() //List내 데이터를 오름차순으로 정렬 
+    answer = myList.toIntArray()
     
     return answer
    }
