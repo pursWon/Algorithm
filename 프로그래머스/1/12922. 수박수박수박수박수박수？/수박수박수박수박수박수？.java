ad832f90dfc5bdeaf761d57a5cc4266fef3ce1c7
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-18 23:53:40 +0900

    Update 수박수박수박수박수박수？.java

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -3,8 +3,10 @@ class Solution {
         String answer = "";
         
         for (int i = 0; i < n; i++) {
+            // i가 0이거나, 짝수일 때 "수"를 더함
             if (i % 2 == 0) {
                 answer += "수";
+            // i가 홀수일 때 "박"을 더함
             } else {
                 answer += "박";
             }
