55384950ed28db82ab91c9dce460238bf97c2f87
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-20 17:07:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.63 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+       Stack<Integer> stack = new Stack();
+
+		for (int i : arr) {
+			if (!stack.empty()) {
+				if (i != stack.peek()) {
+					stack.push(i);
+				} 
+			} else {
+				stack.push(i);
+			}
+		}
+		
+		int[] answer = new int[stack.size()];
+		
+		for (int i = 0; i < stack.size(); i++) {
+			answer[i] = stack.get(i);
+		}
+
+        return answer;
+    }
+}
