c7800c3ca4bb65e8e9d1da6f9971589642399509
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-23 00:16:42 +0900

    Update 과일 장수.java

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
+        // score가 [1, 2, 3, 1, 2, 3, 1]
+        // k = 3
+        // m = 4
         int answer = 0;
 
-        Arrays.sort(score);
+        Arrays.sort(score); // [1, 1, 1, 2, 2, 3, 3]
 
+        // 한 상자에 들어가는 사과의 개수 x 상자의 사과 중 가장 낮은 점수
         for(int i = score.length; i >= m; i -= m){
             answer += score[i - m] * m;
         }
