d43e0833525ab77747f181ac6180b003ba8f14de
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-23 00:18:22 +0900

    Update 음양 더하기.java

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -5,6 +5,7 @@ class Solution {
 
         // signs[i]가 true면 1을 곱하고 (양)
         // signs[i]가 false면 -1을 곱하고 (음)
+        // 삼항연산자를 사용.
         for(int i = 0; i < length; i++) 
         	answer += absolutes[i] * (signs[i] ? 1 : -1);
         
