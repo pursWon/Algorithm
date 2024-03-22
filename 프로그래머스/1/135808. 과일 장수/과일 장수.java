4815f97a51790473625f8085567ed943b6094c71
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-23 00:17:59 +0900

    Update 과일 장수.java

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -10,6 +10,7 @@ class Solution {
         Arrays.sort(score); // [1, 1, 1, 2, 2, 3, 3]
 
         // 한 상자에 들어가는 사과의 개수 x 상자의 사과 중 가장 낮은 점수
+        // 4 x 2 = 8
         for(int i = score.length; i >= m; i -= m){
             answer += score[i - m] * m;
         }
