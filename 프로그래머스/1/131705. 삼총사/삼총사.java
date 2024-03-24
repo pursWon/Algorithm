756d27895e652dd7979945cc67440e8d4724f77b
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-24 13:15:23 +0900

    Update 삼총사.java

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] number) {
         // 단순하게 접근하였습니다.
-		// 배열이 { -2, 3, 0, 2, -5 }라면
-		// -2, 3, 0 더하고, -2, 3, 2 더하고,-2, 3, 5 더하고...
+	// 배열이 { -2, 3, 0, 2, -5 }라면
+	// -2, 3, 0 더하고, -2, 3, 2 더하고,-2, 3, 5 더하고...
         int answer = 0;
         
         for(int i = 0; i < number.length; i ++) {
