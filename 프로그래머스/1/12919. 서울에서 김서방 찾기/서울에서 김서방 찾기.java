43fe49403ee1e92737e76f730b631cc4f0898aec
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-26 01:45:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.05 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        List<String> list = new ArrayList<>(Arrays.asList(seoul));
+        answer = "김서방은 "+list.indexOf("Kim")+"에 있다";
+        
+        System.out.println(answer);
+        
+        return answer;
+    }
+}
