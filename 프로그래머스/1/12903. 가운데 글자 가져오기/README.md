90527865cd40a30dc61cd096dfb3a67b24e1578e
WONGI HONG <99719661+pursWon@users.noreply.github.com>
2024-03-25 17:24:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 2.07 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 1.38 ms
+메모리: 74.6 MB, 시간: 2.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 17:21:05
+2024년 03월 25일 17:24:39
 
 ### 문제 설명
 
--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -3,11 +3,15 @@ class Solution {
         String answer = "";
 		String[] arr = s.split("");
 		int length = arr.length;
-
+        
+        // 짝수
 		if(length % 2 == 0) {
 			answer = arr[(length / 2) - 1] += arr[(length / 2)];
+        // 홀수
 		} else {
+            // 내림을 하기위해 Double로 타입 변경
 			Double number = Double.valueOf(length / 2);
+            // 다시 값을 int로 변환해줌
 			answer = arr[(int)Math.floor(number)];
 		}
         
